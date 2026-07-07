ae2508315a9a681f9316aca36285deb67ccb0327
ihkim848 <gugaf848@gmail.com>
2026-07-07 22:28:30 +0900

    AI 모델 학습

--- backend/modules/classifier.py
+++ backend/modules/classifier.py
@@ -5,21 +5,21 @@ from pytorch_grad_cam import GradCAM
 from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
 from pytorch_grad_cam.utils.image import show_cam_on_image
 import numpy as np
+import os
 from PIL import Image
 
 class SkinDiseaseClassifier:
     def __init__(self):
-        self.model = models.mobilenet_v2(pretrained=True)
+        self.model = models.mobilenet_v2(pretrained=False)
+        
+        num_classes = 20
+        self.model.classifier[1] = torch.nn.Linear(self.model.last_channel, num_classes)
+
+        base_dir = os.path.dirname(os.path.abspath(__file__))
+        model_path = os.path.join(base_dir, 'trained_skin_model.pth')
+
+        self.model.load_state_dict(torch.load(model_path, map_location='cpu'))
         self.model.eval()
-        
-        self.target_layers = [self.model.features[-1]]
-        self.cam = GradCAM(model=self.model, target_layers=self.target_layers)
-        
-        self.preprocess = transforms.Compose([
-            transforms.Resize((224, 224)),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-        ])
 
     def predict_and_explain(self, image_pil):
         input_tensor = self.preprocess(image_pil).unsqueeze(0)
